4f02ec33ef7a67a9a53c864d26db0d0bf58f6645
Siddharth Rao <shindlekar@honorlock.com>
2023-01-23 10:52:09 -0500

    setting headless true

--- playwright.config.ts
+++ playwright.config.ts
@@ -37,7 +37,7 @@ const config: PlaywrightTestConfig = {
     actionTimeout: 0,
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://localhost:3000',
-    headless: false,
+    headless: true,
     viewport: { width: 1280, height: 720 },
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
