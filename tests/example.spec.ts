319acd919e02b8cb06e78b5d01d6b2f1a48352af
Siddharth Rao <shindlekar@honorlock.com>
2023-01-25 12:14:19 -0500

    Getting directory structure

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -24,7 +24,7 @@ test("get started link", async ({ page }) => {
   await page.goto("https://playwright.dev/");
 
   // Click the get started link.
-  await page.getByRole("link", { name: "Get started1" }).click();
+  await page.getByRole("link", { name: "Get started" }).click();
 
   // Expects the URL to contain intro.
   await expect(page).toHaveURL(/.*intro/);
