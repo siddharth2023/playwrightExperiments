04448fcafb32ac24ff2122bdf7be37dd674d428b
Siddharth Rao <shindlekar@honorlock.com>
2023-01-25 15:04:56 -0500

    changing reporter to test-results

--- playwright.config.ts
+++ playwright.config.ts
@@ -30,7 +30,18 @@ const config: PlaywrightTestConfig = {
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  // reporter: 'html',
+  reporter: [
+    ['html', { open: 'never', outputFolder: 'test-results/html' }],
+    ['list'],
+    ['allure-playwright', { outputFolder: 'allure-results' }],
+    [
+        'json',
+        {
+            outputFile: process.env.PW_RESULTS_FILENAME,
+        },
+    ],
+],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
